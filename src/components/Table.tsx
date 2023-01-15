68e9810e50439e16875f6b9e8b7e120798bcd91b
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-01-15 22:27:10 +0900

    react table導入

---
+++ src/components/Table.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Company from "../models/Company";
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+
+/** 表に固定表示するデータ */
+const defaultCompanyData: Company[] = [
+  {
+    name: 'りんゆ株式会社',
+    corporateNumber: 1123456789012,
+    initialCapital: 1000,
+    foundedIn: 2022
+  },
+  {
+    name: '合同会社ミミズク',
+    corporateNumber: 2123456789012,
+    initialCapital: 20000,
+    foundedIn: 1988
+  },
+  {
+    name: '有限会社フクロウ',
+    corporateNumber: 3123456789012,
+    initialCapital: 100,
+    foundedIn: 1999
+  }
+]
+
+const columnHelper = createColumnHelper<Company>()
+
+/** カラム設定 */
+const columns = [
+  columnHelper.accessor('corporateNumber', {
+    cell: info => info.getValue()
+  }),
+  columnHelper.accessor('name', {
+    cell: info => info.getValue()
+  }),
+  columnHelper.accessor('initialCapital', {
+    cell: info => info.getValue()
+  }),
+  columnHelper.accessor('foundedIn', {
+    cell: info => info.getValue()
+  })
+]
+
+function Table() {
+  const [data] = React.useState(() => [...defaultCompanyData])
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
+
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
---
+++ src/models/Company.ts
@@ -0,0 +1,13 @@
+/** 会社情報 */
+type Company = {
+  /** 会社名 */
+  name: string,
+  /** 法人番号 */
+  corporateNumber: number,
+  /** 資本金(万円) */
+  initialCapital: number,
+  /** 設立年 */
+  foundedIn: number
+}
+
+export default Company
